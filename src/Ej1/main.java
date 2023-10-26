034da351cb2040f01131ef92ccee2ce4b78d0b70
FACULTADES\alumnoFI <vdsv@ghfg>
2023-10-26 11:18:56 -0300

    Initial commit

---
+++ src/Ej1/main.java
@@ -0,0 +1,13 @@
+
+package Ej1;
+
+
+public class main {
+
+    
+    public static void main(String[] args) {
+        Ventana vent = new Ventana();
+        vent.setVisible(true);
+    }
+    
+}
---
+++ src/Ej2/Ventanax.java
@@ -0,0 +1,97 @@
+package Ej2;
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
+public class Ventanax extends javax.swing.JFrame {
+
+    
+    public Ventanax() {
+        initComponents();
+        this.setSize(400, 400);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        BotonAutor = new javax.swing.JButton();
+        BotonRojo = new javax.swing.JRadioButton();
+        BotonAzul = new javax.swing.JRadioButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        BotonAutor.setText("Autor");
+        BotonAutor.setToolTipText("");
+        BotonAutor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonAutorActionPerformed(evt);
+            }
+        });
+
+        BotonRojo.setText("Rojo");
+        BotonRojo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonRojoActionPerformed(evt);
+            }
+        });
+
+        BotonAzul.setText("Azul");
+        BotonAzul.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonAzulActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(116, 116, 116)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(BotonAzul)
+                            .addComponent(BotonRojo)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(145, 145, 145)
+                        .addComponent(BotonAutor, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(163, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addComponent(BotonRojo)
+                .addGap(42, 42, 42)
+                .addComponent(BotonAzul)
+                .addGap(64, 64, 64)
+                .addComponent(BotonAutor, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(71, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void BotonRojoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonRojoActionPerformed
+        BotonAutor.setForeground(Color.red);
+    }//GEN-LAST:event_BotonRojoActionPerformed
+
+    private void BotonAutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonAutorActionPerformed
+        JOptionPane.showMessageDialog(this, "Joaquin Larronda", "Autor", JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_BotonAutorActionPerformed
+
+    private void BotonAzulActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonAzulActionPerformed
+        BotonAutor.setForeground(Color.blue);
+    }//GEN-LAST:event_BotonAzulActionPerformed
+    
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotonAutor;
+    private javax.swing.JRadioButton BotonAzul;
+    private javax.swing.JRadioButton BotonRojo;
+    // End of variables declaration//GEN-END:variables
+}
